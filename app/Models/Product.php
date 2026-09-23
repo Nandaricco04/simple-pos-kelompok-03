804adc13e3e1327377bd4d6f39d3031cb16a93f8
RafifFarrelsyahFawwazka <rafifcer123@gmail.com>
2026-09-23 15:14:56 +0700

    tambah relationship details pada product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -19,5 +20,10 @@ class Product extends Model
             Transaction::class,
             'transaction_details'
         );
+    }
+
+    public function details(): HasMany
+    {
+        return $this->hasMany(TransactionDetail::class);
     }
 }
